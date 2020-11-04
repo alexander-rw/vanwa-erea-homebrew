792ac9a2d43014e192ed8881077455545f0c4b44
Alexander Reyes-Wainwright <alexrw17@gmail.com>
2020-11-04 22:15:12 +0000

    Updated

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,6 +1,11 @@
 import { EOL } from "os";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
 import { parseFile, save } from "./functions.js";
 import { meta } from "./meta.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const monsters = [
     "kharid/mactar.json",
