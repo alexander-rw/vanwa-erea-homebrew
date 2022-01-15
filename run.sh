9d4ff43275fc93ad167175468cd21231d52a2492
Alexander Reyes-Wainwright <alexrw17@gmail.com>
2022-01-15 13:55:26 +0000

    Updated

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-deno run --unstable --allow-read --allow-write main.ts --fileName vanwa-erea-meta.json --debug false
+deno run --unstable --allow-read --allow-write main.ts --fileName vanwa-erea-meta.json --debug false --dry-run true
 git add --all
 git commit -m "Updated"
 git push
