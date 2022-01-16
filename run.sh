c0aeedd185dfdf6efd73795029ddf2e373ef7a83
Alexander Reyes-Wainwright <alexrw17@gmail.com>
2022-01-16 13:05:51 +0000

    Updated

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ git add --all
 git commit -m "Updated"
 git push
 echo "File available at:"
+echo ""
 echo "https://raw.githubusercontent.com/alexander-rw/vanwa-erea-homebrew/main/vanwa-erea-meta.json"
+echo ""
